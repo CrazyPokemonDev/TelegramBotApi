3d719ee58e56badc6861ea875203b049c366872e
Olfi01 <flom@meyer-alpers.de>
2018-06-22 22:08:24 +0200

    Added the first few methods as well as update getting

---
+++ TelegramBotApi/Types/Events/UpdateEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBotApi.Types.Events
+{
+    public class UpdateEventArgs : EventArgs
+    {
+
+    }
+}

d25bd98e2413ce8d1e011841b1f2295dd3da1c92
Florian Meyer <florian.meyer@capgemini.com>
2019-01-14 09:42:03 +0100

    Added config file for integration testing

---
+++ TelegramBotApiUnitTest/Integ/IntegTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TelegramBotApi;
+
+namespace TelegramBotApiUnitTest.Integ
+{
+    [TestClass]
+    public class IntegTests
+    {
+        protected TelegramBot Bot;
+        protected IntegrationConfiguration Config;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            Bot = new TelegramBot(Config.BotToken);
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+
+        }
+    }
+}
